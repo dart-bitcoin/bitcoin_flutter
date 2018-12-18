f9a2cb4c1567f3d39715349f27a8c1b2f430b2a3
Don Gunn <itsMikeLowrey@users.noreply.github.com>
2018-12-18 04:12:13 -0800

    Added Data types and seperated classes

--- lib/src/payments/p2ms.dart
+++ lib/src/payments/p2ms.dart
@@ -1,7 +1,13 @@
+import '../utils/constants/op.dart' show OP_RESERVED;
+import 'package:meta/meta.dart';
 
-import '../utils/constants/op.dart';
 
 class P2MS {
+  P2MSData data;
+
+  P2MS({@required data}) {
+    this.data = data;
+  }
 
   bool stacksEqual (a, b) {
     if (a.length != b.length) return false;
@@ -12,4 +18,29 @@ class P2MS {
     }
     return true;
   }
+}
+
+
+class P2MSData {
+  int m;
+  int n;
+  String output;
+  String input;
+  List pubkeys;
+  List signatures;
+  Map options;
+
+  P2MSData(
+      {this.m,
+      this.n,
+      this.output,
+      this.input,
+      this.pubkeys,
+      this.signatures});
+
+/*   @override
+  String toString() {
+    return 'P2MSData{address: $address, hash: $hash, output: $output, signature: $signature, pubkey: $pubkey, input: $input}';
+  } */
+
 }

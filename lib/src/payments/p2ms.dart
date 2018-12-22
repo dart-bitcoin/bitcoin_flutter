ccb9980b441c121c2704f27549e6fe49edb4ed6e
Don Gunn <itsMikeLowrey@users.noreply.github.com>
2018-12-22 02:27:26 -0800

    Working on lazy eval

--- lib/src/payments/p2ms.dart
+++ lib/src/payments/p2ms.dart
@@ -1,15 +1,42 @@
-import '../utils/constants/op.dart' show OP_RESERVED;
+import '../utils/constants/op.dart';
 import 'package:meta/meta.dart';
+import '../utils/script.dart' as bscript;
+import '../models/networks.dart';
+
+
 
 
 class P2MS {
   P2MSData data;
+  NetworkType network;
+  List<dynamic> _chunks;
 
   P2MS({@required data}) {
     this.data = data;
+    this.network = network ?? bitcoin;
   }
-
-  bool stacksEqual (a, b) {
+  get _tempItem {1 + 2;} 
+  void _enoughInformation(data) {
+    if (
+    !data.input &&
+    !data.output &&
+    !(data.pubkeys && data.m != null) &&
+    !data.signatures 
+    ) throw FormatException('Not enough data');
+  }
+  void _extraValidation(options){
+    options['validate'] = true;
+  }
+  bool _isAcceptableSignature(signature,options) {
+    return bscript.isCanonicalScriptSignature(signature) || (options.allowIncomplete && (signature == OPS['OP_0']));
+  }
+  void _decode(output) {
+    _chunks = bscript.decompile(output);
+    _tempItem.m = _chunks[0] - OPS['OP_INT_BASE'];
+    _tempItem.n = _chunks[_chunks.length - 2] - OPS['OP_INT_BASE'];
+    _tempItem.pubkeys = _chunks.sublist(1, -3);
+  }
+  bool _stacksEqual(a, b) {
     if (a.length != b.length) return false;
     for(int i=1;i<=a.length;i++) {
       if (a[i]!=b[i]) {
@@ -18,8 +45,8 @@ class P2MS {
     }
     return true;
   }
+  
 }
-
 
 class P2MSData {
   int m;
@@ -30,17 +57,21 @@ class P2MSData {
   List signatures;
   Map options;
 
-  P2MSData(
+  P2MSData( 
       {this.m,
       this.n,
       this.output,
       this.input,
       this.pubkeys,
-      this.signatures});
-
-/*   @override
+      this.signatures,
+      this.options});
+  @override
   String toString() {
-    return 'P2MSData{address: $address, hash: $hash, output: $output, signature: $signature, pubkey: $pubkey, input: $input}';
-  } */
+    return 'P2MSData{sigs: $m, neededSigs: $n, output: $output, input: $input, pubkeys: $pubkeys, sigs: $signatures, options: $signatures}';
+  } 
 
 }
+
+
+  
+

014182049e91efab8145872b2def4b757ffc0308
Don Gunn <itsMikeLowrey@users.noreply.github.com>
2019-01-20 20:31:36 -0800

    Removed _temp, but fails 3 test
    fails these 3 test starting at lines 146

--- lib/src/payments/p2ms.dart
+++ lib/src/payments/p2ms.dart
@@ -9,14 +9,12 @@ class P2MS {
   P2MSData data;
   NetworkType network;
   List<dynamic> _chunks;
-  Map _temp;
   bool _isDecoded;
 
   P2MS({@required data}) {
     this.data = data;
     this.network = network ?? bitcoin;
     this._isDecoded = false;
-    this._temp = {};
     _init();
   }
   void _init() {
@@ -35,7 +33,7 @@ class P2MS {
     }
   }
   void _setNetwork(network){
-      _temp['network'] == network;
+      this.network == network;
   }
   void _extraValidation(options) {
     if(data.options == null){data.options = {};}
@@ -46,7 +44,6 @@ class P2MS {
     if(data.options['validate']==true){
       _check();
     }
-    _assignVariables();
   }
    void _decode(output){
      
@@ -54,20 +51,20 @@ class P2MS {
     else{
       _isDecoded = true;
       _chunks = bscript.decompile(output);
-      _temp['m'] = _chunks[0] - OPS['OP_RESERVED'];
-      _temp['n'] = _chunks[_chunks.length - 2] - OPS['OP_RESERVED'];
-      _temp['pubkeys'] = _chunks.sublist(1,_chunks.length-2);
+      data.m = _chunks[0] - OPS['OP_RESERVED'];
+      data.n = _chunks[_chunks.length - 2] - OPS['OP_RESERVED'];
+      data.pubkeys = _chunks.sublist(1,_chunks.length-2);
     }
   }
   void _setOutput(){
     if (data.m == null){ return;}
-    if (_temp['n'] == null) {return;}
+    if (data.n == null) {return;}
     if (data.pubkeys == null) {return;}
     List<dynamic> list = [OPS['OP_RESERVED']+data.m];
     data.pubkeys.forEach((pubkey) => list.add(pubkey));
-    list.add(OPS['OP_RESERVED'] + _temp['n']);
+    list.add(OPS['OP_RESERVED'] + data.n);
     list.add(OPS['OP_CHECKMULTISIG']);
-    _temp['output'] = bscript.compile(list);
+    data.output = bscript.compile(list);
   }
   void _setSigs(){
     if (data.input == null) {return;}
@@ -85,7 +82,7 @@ class P2MS {
       _chunks.add(temp);
     }
     
-    _temp['signatures'] = _chunks;
+    data.signatures = _chunks;
   }
   void _setInput(){
     if (data.signatures == null) {return;}
@@ -100,35 +97,26 @@ class P2MS {
     }
     tempString = tempString+(bscript.toASM(tempsignatures));
     Uint8List tempList = bscript.fromASM(tempString);
-    _temp['input'] = bscript.compile(tempList);  
+    data.input = bscript.compile(tempList);  
   }
   void _setWitness(){
-    if (_temp['input'] == null && data.input == null) {return;}
+    if (data.input == null && data.input == null) {return;}
     List <Uint8List> temp = [];
-    _temp['witness'] = temp;
+    data.witness = temp;
   }
 
   void _setM(){
-    if (_temp['output'] == null) {return;}
-    _decode(_temp['output']);
+    if (data.output == null) {return;}
+    _decode(data.output);
   }
     void _setN(){
     _setPubkeys();
-    if (_temp['pubkeys'] == null) {return;}
-    _temp['n']= _temp['pubkeys'].length;
+    if (data.pubkeys == null) {return;}
+    data.n= data.pubkeys.length;
   }
     void _setPubkeys(){
     if (data.output == null) {return;}
     _decode(data.output);
-  }
-  void _assignVariables(){
-    if (_temp['m'] != null) {data.m = _temp['m'];}
-    if (_temp['n'] != null) {data.n = _temp['n'];}
-    if (_temp['output'] != null) {data.output = _temp['output'];}
-    if (_temp['pubkeys'] != null) {data.pubkeys = _temp['pubkeys'];}
-    if (_temp['signatures'] != null) {data.signatures = _temp['signatures'];}
-    if (_temp['input'] != null) {data.input= _temp['input'];}
-    if (_temp['witness'] != null) {data.witness= _temp['witness'];}
   }
     bool _stacksEqual(a, b) {
     if (a.length != b.length) {return false;}
@@ -150,37 +138,37 @@ class P2MS {
       if (temp.length < 2 ) {throw new ArgumentError('Output is invalid');}
       if (temp[temp.length - 1] != OPS['OP_CHECKMULTISIG']) {throw new ArgumentError('Output is invalid');}
       _decode(data.output);
-    if(_temp['m'] <= 0 ||
-        _temp['n'] > 16 ||
-        _temp['m'] > _temp['n'] ||
-        _temp['n'] != _chunks.length - 3) {throw new ArgumentError('Output is invalid');}
-    if (!_temp['pubkeys'].every((x) => isPoint(x))) {throw new ArgumentError('Output is invalid');}
-    if (data.m != null && data.m != _temp['m']) {throw new ArgumentError('m mismatch');}
-    if (data.n != null && data.n != _temp['n']) {throw new ArgumentError('n mismatch');}
-    if (data.pubkeys != null && !_stacksEqual(data.pubkeys, _temp['pubkeys'])) {throw new ArgumentError('Pubkeys mismatch');}
+    if(data.m <= 0 ||
+        data.n > 16 ||
+        data.m > data.n ||
+        data.n != _chunks.length - 3) {throw new ArgumentError('Output is invalid');}
+    if (!data.pubkeys.every((x) => isPoint(x))) {throw new ArgumentError('Output is invalid');}
+    if (data.m != null && data.m != data.m) {throw new ArgumentError('m mismatch');}
+    if (data.n != null && data.n != data.n) {throw new ArgumentError('n mismatch');}
+    if (data.pubkeys != null && !_stacksEqual(data.pubkeys, data.pubkeys)) {throw new ArgumentError('Pubkeys mismatch');}
   }
   if (data.pubkeys != null){
     if (data.n != null && data.n != data.pubkeys.length) {throw new ArgumentError('Pubkey count mismatch');}
-    _temp['n'] = data.pubkeys.length;
+    data.n = data.pubkeys.length;
     _setOutput();
     _setM();
     _setN();
-    if (_temp['n'] < _temp['m']) {throw new ArgumentError('Pubkey count cannot be less than m');}
+    if (data.n < data.m) {throw new ArgumentError('Pubkey count cannot be less than m');}
   }
   if (data.signatures != null) {
     _setSigs();
     _setInput();
-    if (data.signatures.length < _temp['m']) {throw new ArgumentError('Not enough signatures provided');}
-    if (data.signatures.length > _temp['m']) {throw new ArgumentError('Too many signatures provided');}
+    if (data.signatures.length < data.m) {throw new ArgumentError('Not enough signatures provided');}
+    if (data.signatures.length > data.m) {throw new ArgumentError('Too many signatures provided');}
     }
 
     
   if (data.input != null) {
       if (data.input[0] != OPS['OP_0']) {throw new ArgumentError('Input is invalid');}
       _setSigs();
-      if (_temp['signatures'].length == 0 || !_temp['signatures'].every((x) => _isAcceptableSignature(x,data.options)))
+      if (data.signatures.length == 0 || !data.signatures.every((x) => _isAcceptableSignature(x,data.options)))
       {throw new ArgumentError('Input has invalid signature(s)');}
-      if (data.signatures != null&& !_stacksEqual(data.signatures,_temp['signatures'])) {throw new ArgumentError('Signature mismatch');}
+      if (data.signatures != null&& !_stacksEqual(data.signatures,data.signatures)) {throw new ArgumentError('Signature mismatch');}
       if (data.m != null && data.m != data.signatures.length) {throw new ArgumentError('Signature count mismatch');}
     }
   _setInput();

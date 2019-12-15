59350141b51f14850144df376f0bab84147a25b3
SFzxc <hoangphuocvanlong@gmail.com>
2019-12-15 10:25:22 +0700

    Add backward compalibility for P2PKHData

--- lib/src/payments/index.dart
+++ lib/src/payments/index.dart
@@ -27,5 +27,14 @@ class PaymentData {
 
 @Deprecated('The "P2PKHData" class is deprecated. Use the "PaymentData" package instead.')
 class P2PKHData extends PaymentData {
-  super({address, hash, output, pubkey, input, signature, witness});
+  P2PKHData({address, hash, output, pubkey, input, signature, witness}) :
+    super(
+        address: address,
+        hash: hash,
+        output: output,
+        pubkey: pubkey,
+        input: input,
+        signature: signature,
+        witness: witness
+    );
 }

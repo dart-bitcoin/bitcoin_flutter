8795f804cf93ebbcb60d75cc87ba6b3298425e23
willy <pussinbootsdev@gmail.com>
2021-02-20 18:17:29 +0100

    add peercoin to network models

--- lib/src/models/networks.dart
+++ lib/src/models/networks.dart
@@ -49,3 +49,19 @@ final testnet = new NetworkType(
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     wif: 0xef);
+
+final peercoin = new NetworkType(
+    messagePrefix: '\x18Peercoin Signed Message:\n',
+    bech32: 'pc',
+    bip32: new Bip32Type(public: 0x80000006, private: 0x0488ade4),
+    pubKeyHash: 0x37,
+    scriptHash: 0x75,
+    wif: 0xb7);
+
+final peercoinTestnet = new NetworkType(
+    messagePrefix: '\x18Peercoin Signed Message:\n',
+    bech32: 'tpc',
+    bip32: new Bip32Type(public: 0x80000006, private: 0x0488ade4),
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef);

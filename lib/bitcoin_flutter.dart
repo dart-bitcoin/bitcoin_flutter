97d99960e2f64a4e8161266b6d29576dfb2ada7a
Sergey Fedorov <fedser@appswithlove.com>
2021-07-17 00:17:39 +0400

    Remove transactions files

--- lib/bitcoin_flutter.dart
+++ lib/bitcoin_flutter.dart
@@ -1,13 +1,12 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
+// @dart=2.9
 library bitcoin_flutter;
 
 export 'src/bitcoin_flutter_base.dart';
 export 'src/models/networks.dart';
-export 'src/transaction.dart';
 export 'src/address.dart';
-export 'src/transaction_builder.dart';
 export 'src/ecpair.dart';
 export 'src/payments/p2pkh.dart';
 export 'src/payments/p2wpkh.dart';

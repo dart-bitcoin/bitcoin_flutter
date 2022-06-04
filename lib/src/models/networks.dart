e7859445903991bd9f19439dc14052bcb57c135d
TheArchitect <TheArchitect108@protonmail.com>
2022-06-04 11:32:45 -0500

    adds regtest

--- lib/src/models/networks.dart
+++ lib/src/models/networks.dart
@@ -49,3 +49,11 @@ final testnet = new NetworkType(
     pubKeyHash: 0x6f,
     scriptHash: 0xc4,
     wif: 0xef);
+
+final regtest = new NetworkType(
+    messagePrefix: '\x18Bitcoin Signed Message:\n',
+    bech32: 'bcrt',
+    bip32: new Bip32Type(public: 0x043587cf, private: 0x04358394),
+    pubKeyHash: 0x6f,
+    scriptHash: 0xc4,
+    wif: 0xef);
